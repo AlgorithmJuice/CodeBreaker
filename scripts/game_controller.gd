3324e91a38109382d31c26ad04409fb4dba7fe6e
Gavin Youker <youkergav@gmail.com>
2024-03-13 00:42:49 -0400

    added round timer

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -3,18 +3,29 @@ extends Node
 
 @export var passphrase: Array = []
 @export var current_round: int = 0
+@export var round_time: float = 30
 
 var total_rounds: int = 0
+var passphrase_label: Label = null
+var countdown_label: Label = null
+var countdown_timer: Timer = null
 
 func _ready():
 	passphrase = get_passphrase()
 	total_rounds = passphrase.size()
 	
-	get_node("Passcode/PasscodeLabel").text = display_passphrase()
-	
-func _on_passcode_timer_timeout():
-	get_node("Passcode/PasscodeLabel").text = display_passphrase()
-	
+	init_passphrase()
+	init_countdown()
+
+func _process(_delta):
+	countdown_label.text = display_countdown()
+
+func _on_passphrase_timer_timeout():
+	passphrase_label.text = display_passphrase()
+
+func _on_countdown_timer_timeout():
+	pass # Will be used later to reset round state.
+
 func get_passphrase():
 	var phrase = GameData.phrases[randi() % GameData.phrases.size()]	
 	return phrase.split(" ")
@@ -36,3 +47,18 @@ func display_passphrase():
 		cracked_passphrase += " "
 	
 	return cracked_passphrase.left(-1)
+
+func init_passphrase():
+	passphrase_label = $MarginContainer/HorizontalContainer/PassphraseLabel
+	passphrase_label.text = display_passphrase()
+
+func display_countdown():
+	return "%.2f" % countdown_timer.time_left
+
+func init_countdown():
+	countdown_label = $MarginContainer/HorizontalContainer/CountdownLabel
+	countdown_timer = $MarginContainer/HorizontalContainer/CountdownLabel/CountdownTimer
+	
+	countdown_timer.wait_time = round_time
+	countdown_timer.start()
+	countdown_label.text = display_countdown()
