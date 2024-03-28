0eccb68e4543dceaec77a05948cadd17840d41a9
Nate-Perkins <perkinsnathan97@gmail.com>
2024-03-28 16:57:37 -0400

    SFX in 3 spots & music

--- scripts/player.gd
+++ scripts/player.gd
@@ -20,6 +20,11 @@ var current_wordlist: Array:
 var total_rounds: int:
 	get: return correct_passphrase.size()
 
+
+signal timer_out
+signal wrong_word
+signal correct_word
+
 func init():
 	_init_passphrase()
 	_init_timer()
@@ -72,6 +77,7 @@ func _on_input_gamepad():
 func _on_timer_timeout():
 	# TODO: Fix timer timeout by penalty.
 	matrix.reset()
+	timer_out.emit()
 	timer.stop()
 	timer.wait_time = round_time
 	timer.start()
@@ -82,6 +88,7 @@ func _handle_correct_word():
 	if current_round == total_rounds:
 		get_tree().change_scene_to_file("res://scenes/final.tscn")
 	else:
+		correct_word.emit()
 		matrix.words = current_wordlist
 		matrix.reset()
 		passphrase.current_round = current_round
@@ -92,6 +99,7 @@ func _handle_leet_word():
 	
 func _handle_wrong_word(word):
 	var found_chars = _compare_characters(word, current_password)
+	wrong_word.emit()
 	matrix.add_known_chars(found_chars)
 	matrix.add_inactive_word(word)
 	matrix.reload()
--- scripts/sfx_player.gd
+++ scripts/sfx_player.gd
@@ -13,9 +13,27 @@ func _process(_delta):
 func _init_sfx(player_id):
 	if(player_id == 1):
 		$TypeSfx.bus = "Left"
+		$CorrectSfx.bus = "Left"
+		$BeepSfx.bus = "Left"
+		$TimerSfx.bus = "Left"
 	elif(player_id == 2):
 		$TypeSfx.bus = "Right"
+		$CorrectSfx.bus = "Right"
+		$BeepSfx.bus = "Right"
+		$TimerSfx.bus = "Right"
 
 func _on_matrix_selection_change():
 	$TypeSfx.play()
+	
+func _on_timer_reset():
+	$TimerSfx.play()
+	
+func _on_wrong_word():
+	$BeepSfx.play()
+
+func _on_correct_word():
+	$CorrectSfx.play()
+	
+	
+	
 
