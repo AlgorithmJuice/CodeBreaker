7c333215325aee35817ed5e1884083ebb77ae55c
Gavin Youker <youkergav@gmail.com>
2024-03-28 01:19:56 -0400

    Added init splash screen

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,5 +1,8 @@
 extends Control
 
+@export var player1: Button
+@export var player2: Button
 
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+func _process(_delta):
+	if player1.active and player2.active:
+		get_tree().change_scene_to_file("res://scenes/game.tscn")
---
+++ scripts/play_button.gd
@@ -0,0 +1,34 @@
+extends Button
+
+@export var id: int = 0
+var active: bool = false
+var flash: bool = false
+var themes: Dictionary = {
+	"normal": preload("res://themes/play_button_normal.tres"),
+	"flash": preload("res://themes/play_button_flash.tres")
+}
+
+func _process(_delta):
+	_on_input()
+
+func _on_timer_timeout():
+	if active:
+		self.set_theme(themes["flash"])
+		return
+	
+	flash = !flash
+	
+	if flash:
+		self.set_theme(themes["flash"])
+	else:
+		self.set_theme(themes["normal"])
+
+func _on_input():
+	if Input.is_action_just_pressed("bgs_start_p%d" % id):
+		active = !active
+		
+		if active:
+			self.set_theme(themes["normal"])
+			self.text = "Player %d Ready" % id
+		else:
+			self.text = "Player %d Start" % id
